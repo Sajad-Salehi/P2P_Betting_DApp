faa36f16a9c65d0bf1b38ab812339091a57e72ae
Sajad-Salehi <SajadSolidity@gmail.com>
2023-01-30 19:15:36 +0330

    added componetns

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useAccount, useConnect, useDisconnect, useProvider, useSigner } from 'wagmi'
+
+
+export default function Dashboard(): JSX.Element {
+    return(
+        
+        <></>
+    );
+}
