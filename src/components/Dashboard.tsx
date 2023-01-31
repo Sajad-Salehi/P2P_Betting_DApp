28aef0656de0080d469c437379847a78ba9ff3e2
Sajad-Salehi <SajadSolidity@gmail.com>
2023-01-31 23:03:06 +0330

    updated dashboard style

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,58 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useAccount, useConnect, useDisconnect, useProvider, useSigner } from 'wagmi'
+import { useAccount, useConnect, useContractRead, useSigner  } from 'wagmi'
+import { useState, useEffect } from 'react';
+const {abi} = require('../abi.json')
 
 
 export default function Dashboard(): JSX.Element {
+
+    const [isClicked, setClicked] = useState(false)
+
+
+    function hi() {
+        setClicked(true)
+    }
+
     return(
         
-        <></>
+        
+        <div className={styles.dashboard}>
+
+            <h1 className={styles.dsh1}>Aryan Bet Dashboard</h1>
+            <div><ConnectButton /></div>
+
+            <main className={styles.main}>
+            
+    
+              <a onClick={hi} className={styles.card}>
+                <h2>See Available Bets Info &rarr;</h2>
+                <h4>
+                  See open bets and accept one to start.
+                </h4>
+              </a>
+    
+    
+              <a className={styles.card}>
+                <h2>Publish Your Bet &rarr;</h2>
+                <h4>
+                  Publish and Open Your Specific Bet.
+                </h4>
+              </a>
+
+
+              <a className={styles.card}>
+                <h2>See Your Current Bet &rarr;</h2>
+                <h4>
+                  See your Current bets info.
+                </h4>
+              </a>
+  
+          
+          </main>
+        </div>
+
+      
     );
 }
