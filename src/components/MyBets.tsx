ea15468e42d3ba21c6544563283540fe5e72fee0
Sajad Salehi <sajadsolidity@gmail.com>
2023-03-19 20:50:10 +0330

    update contract address

--- src/components/MyBets.tsx
+++ src/components/MyBets.tsx
@@ -43,7 +43,7 @@ const MyBets: React.FC = () => {
   const [bets, setBets] = useState<Bet[]>([]);
   const [betId, setBetId] = useState('');
   const { data: signer, isError: signerError, isLoading: signerLoading } = useSigner();
-  const ContractAddress = '0xF1B54387041B54f288e4aa0538aFaA594FA0F1e6';
+  const ContractAddress = '0x27b63A915066610512CDD8748d00FCB25407Bf29';
 
   const handleClick = async (gameId: number) => {
     try {
